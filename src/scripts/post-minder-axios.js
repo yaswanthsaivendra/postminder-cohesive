f6eccf2cbe97dc2040d980a9b2b0c5c3d86d3898
yaswanthsaivendra <yaswanthsaivendra@gmail.com>
2023-03-01 20:35:55 +0530

    changing baseUrl to backend

--- src/scripts/post-minder-axios.js
+++ src/scripts/post-minder-axios.js
@@ -8,7 +8,7 @@ export function getAxiosForPostminder(){
         return postMinderAxios;
     }
     postMinderAxios = getAxios();
-    postMinderAxios = postMinderAxios.create({baseURL : "https://postminder-backend-apps-by-cohesive-0fyz.live.cohesive.so"})
+    postMinderAxios = postMinderAxios.create({baseURL : "https://postminder-backend-yaswanthsaivendra-79cu.live.cohesive.so"})
     console.log(process.env)
     return postMinderAxios;
 }
